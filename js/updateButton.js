f86a63a3ac8aa291170f5d41fdad1171d5fa863a
NotWeelee <44516414+NotWeelee@users.noreply.github.com>
2020-02-05 17:22:16 -0600

    Added some Javascript
    Added javascript to change hashButton text.

---
+++ js/updateButton.js
@@ -0,0 +1,15 @@
+function updateButtonMD5() {
+    document.getElementById("hashButton").innerHTML="HASH USING MD5!";
+}
+
+function updateButtonSHA1() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-1!";
+}
+
+function updateButtonSHA2() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-2!";
+}
+
+function updateButtonSHA3() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-3!";
+}

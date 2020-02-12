de71ff77320fe86bc8e2e7d3de30fa37269d575c
Zhang, Willie <willie-zhang@uiowa.edu>
2020-02-11 18:59:03 -0600

    Created Password Generator
    updated generatePassword.js to fill password input using a dictionary and some extra text files.

--- js/hash.js
+++ js/hash.js
@@ -1,3 +1,20 @@
+var md5 = false;
+var sha1 = false;
+var sha2 = false;
+var sha3 = false;
+
 function hashMD5() {
-    
+    document.getElementById("hashButton").innerHTML="HASH USING MD5!";
+}
+
+function hashSHA1() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-1!";
+}
+
+function hashSHA2() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-2!";
+}
+
+function hashSHA3() {
+    document.getElementById("hashButton").innerHTML="HASH USING SHA-3!";
 }

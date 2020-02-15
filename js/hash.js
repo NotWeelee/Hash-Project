3519af79bc4a74fdcd2d0cfe157ee45cc43b4ed2
NotWeelee <44516414+NotWeelee@users.noreply.github.com>
2020-02-14 19:04:05 -0600

    Added Hashing Algs
    Implemented each hashing algorithm in javascript

--- js/hash.js
+++ js/hash.js
@@ -1,13 +1,18 @@
 var sha256 = false;
 var md5 = false;
 var ripemd = false;
-var blake2 = false;
+var whirlpool = false;
+var words;
+
+function setup() {
+    words = loadStrings("/textFiles/USAndSpanishWords");
+}
 
 function updateSHA256() {
     sha256 = true;
     md5 = false;
     ripemd = false;
-    blake2 = false;
+    whirlpool = false;
     document.getElementById("hashButton").innerHTML="HASH USING SHA-256!";
 }
 
@@ -15,7 +20,7 @@ function updateMD5() {
     sha256 = false;
     md5 = true;
     ripemd = false;
-    blake2 = false;
+    whirlpool = false;
     document.getElementById("hashButton").innerHTML="HASH USING MD5!";
 }
 
@@ -23,39 +28,70 @@ function updateRIPEMD() {
     sha256 = false;
     md5 = false;
     ripemd = true;
-    blake2 = false;
+    whirlpool = false;
     document.getElementById("hashButton").innerHTML="HASH USING RIPEMD!";
 }
 
-function updateBLAKE2() {
+function updateWHIRLPOOL() {
     sha256 = false;
     md5 = false;
     ripemd = false;
-    blake2 = true;
-    document.getElementById("hashButton").innerHTML="HASH USING BLAKE2!";
+    whirlpool = true;
+    document.getElementById("hashButton").innerHTML="HASH USING WHIRLPOOL!";
 }
 
 function hashIt() {
-    if(sha256 == false && md5 == false && ripemd == false && blake2 == false) {
-        $('.blinking').fadeOut(200);
-        $('.blinking').fadeIn(200);
-        $('.blinking').fadeOut(200);
-        $('.blinking').fadeIn(200);
+    if(sha256 == false && md5 == false && ripemd == false && whirlpool == false) {
+        $('.blink').fadeOut(200);
+        $('.blink').fadeIn(200);
+        $('.blink').fadeOut(200);
+        $('.blink').fadeIn(200);
         setInterval(blinker, 500);
     }
     else if(sha256) {
-        var hashed = forge_sha256(document.getElementById("password").value);
-        document.getElementById("plainPassword").innerHTML = document.getElementById("password").value;
+        var password = document.getElementById("password").value;
+        var hashed = new Hashes.SHA256().hex(password);
+        document.getElementById("plainPassword").innerHTML = password;
         document.getElementById("hashedPassword").innerHTML = hashed;
         document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING SHA-256:";
+        
+        //INITIALIZE TIME STUFF
+        for(var i=0; i < words.length; i++) {
+            if(words[i] == password) {
+                //END TIME
+            }
+            for(var j=0; j < 9999; j++) {
+                if((words[i] + j) == password) {
+                    //END TIME
+                }
+            }
+            for(var l=0; l < 1000; l++) {
+                var paddedNum = ('000'+n).slice(-4);
+                if((words[i] + l) == password) {
+                    //END TIME STUFF
+                }
+            }
+        }
     }
     else if(md5) {
-       
+        var password = document.getElementById("password").value;
+        var hashed = new Hashes.MD5().hex(password);
+        document.getElementById("plainPassword").innerHTML = password;
+        document.getElementById("hashedPassword").innerHTML = hashed;
+        document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING MD5:";
     }
     else if(ripemd) {
-       
+        var password = document.getElementById("password").value;
+        var hashed = new Hashes.RMD160().hex(password);
+        document.getElementById("plainPassword").innerHTML = document.getElementById("password").value;
+        document.getElementById("hashedPassword").innerHTML = hashed;
+        document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING RIPEMD-160:";
     }
-    else if(blake2) {
-       
+    else if(whirlpool) {
+        var password = document.getElementById("password").value;
+        var hashed = Whirlpool(password);
+        document.getElementById("plainPassword").innerHTML = password;
+        document.getElementById("hashedPassword").innerHTML = hashed;
+        document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING WHIRLPOOL:";
     }
 }

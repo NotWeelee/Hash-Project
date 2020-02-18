501cddf940f14a061c958f7cf8c55656549bde09
Zhang, Willie <willie-zhang@uiowa.edu>
2020-02-17 21:41:47 -0600

    Updates

--- js/hash.js
+++ js/hash.js
@@ -3,9 +3,14 @@ var md5 = false;
 var ripemd = false;
 var whirlpool = false;
 var words;
+var timeStart, timeEnd;
+var date = new Date();
 
 function setup() {
     words = loadStrings("/textFiles/USAndSpanishWords");
+    validChars = loadStrings('textFiles/validPassChar.txt');
+    alphabet = loadStrings('textFiles/alphabet.txt');
+    all = loadStrings('textFiles/charNumSpecial.txt');
 }
 
 function updateSHA256() {
@@ -55,21 +60,26 @@ function hashIt() {
         document.getElementById("hashedPassword").innerHTML = hashed;
         document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING SHA-256:";
         
-        //INITIALIZE TIME STUFF
+        timeStart = date.getTime();
+        
         for(var i=0; i < words.length; i++) {
             if(words[i] == password) {
-                //END TIME
+                timeEnd = date.getTime();
+                document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
             }
             for(var j=0; j < 9999; j++) {
                 if((words[i] + j) == password) {
-                    //END TIME
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
                 }
             }
             for(var l=0; l < 1000; l++) {
-                var paddedNum = ('000'+n).slice(-4);
+                var paddedNum = ('000'+ l).slice(-4);
                 if((words[i] + l) == password) {
-                    //END TIME STUFF
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
                 }
+
             }
         }
     }
@@ -79,6 +89,29 @@ function hashIt() {
         document.getElementById("plainPassword").innerHTML = password;
         document.getElementById("hashedPassword").innerHTML = hashed;
         document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING MD5:";
+        
+        timeStart = date.getTime();
+        
+        for(var i=0; i < words.length; i++) {
+            if(words[i] == password) {
+                timeEnd = date.getTime();
+                document.getElementById("attackTime").innerHTML = timeStart;
+            }
+            for(var j=0; j < 9999; j++) {
+                if((words[i] + j) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+            }
+            for(var l=0; l < 1000; l++) {
+                var paddedNum = ('000'+ l).slice(-4);
+                if((words[i] + l) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+
+            }
+        }
     }
     else if(ripemd) {
         var password = document.getElementById("password").value;
@@ -86,6 +119,29 @@ function hashIt() {
         document.getElementById("plainPassword").innerHTML = document.getElementById("password").value;
         document.getElementById("hashedPassword").innerHTML = hashed;
         document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING RIPEMD-160:";
+        
+        timeStart = date.getTime();
+        
+        for(var i=0; i < words.length; i++) {
+            if(words[i] == password) {
+                timeEnd = date.getTime();
+                document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+            }
+            for(var j=0; j < 9999; j++) {
+                if((words[i] + j) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+            }
+            for(var l=0; l < 1000; l++) {
+                var paddedNum = ('000'+ l).slice(-4);
+                if((words[i] + l) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+
+            }
+        }
     }
     else if(whirlpool) {
         var password = document.getElementById("password").value;
@@ -93,5 +149,28 @@ function hashIt() {
         document.getElementById("plainPassword").innerHTML = password;
         document.getElementById("hashedPassword").innerHTML = hashed;
         document.getElementById("yourHashedPassword").innerHTML = "HASHED PASSWORD USING WHIRLPOOL:";
+        
+        timeStart = date.getTime();
+        
+        for(var i=0; i < words.length; i++) {
+            if(words[i] == password) {
+                timeEnd = date.getTime();
+                document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+            }
+            for(var j=0; j < 9999; j++) {
+                if((words[i] + j) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+            }
+            for(var l=0; l < 1000; l++) {
+                var paddedNum = ('000'+ l).slice(-4);
+                if((words[i] + l) == password) {
+                    timeEnd = date.getTime();
+                    document.getElementById("attackTime").innerHTML = (timeEnd - timeStart) + " milliseconds";
+                }
+
+            }
+        }
     }
 }
